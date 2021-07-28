38ab5cf876d0c0fcad012e9d6d0ddec336cbc7e7
Roberto Fortes <robertbernd23@gmail.com>
2021-07-28 03:31:59 -0300

    Configurando express

---
+++ backend/src/config/server.js
@@ -0,0 +1,15 @@
+const port = 3003
+
+const express = require('express')
+const server = express()
+const allowCors = require('./cors')
+
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+server.use(allowCors)
+
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
