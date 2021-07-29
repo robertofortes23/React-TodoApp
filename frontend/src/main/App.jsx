e0fde9e4ae7433518dcd5c1fd9c1cb2c612a97ce
Roberto Fortes <robertbernd23@gmail.com>
2021-07-29 09:28:44 -0300

    Componente App

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+require('bootstrap/dist/css/bootstrap.min.css')
+require('font-awesome/css/font-awesome.min.css')
+
+
+const App = props => (
+    <div className="container">
+        <h1>Teste</h1>
+    </div>
+)
+export default App
