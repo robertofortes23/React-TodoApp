d21045733676b86b8b0515e61ff3c284c8e6132f
Roberto Fortes <robertbernd23@gmail.com>
2021-07-29 09:46:29 -0300

    Componente menu

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Todo from '../todo/Todo'
 import About from '../about/About'
+import Menu from '../template/Menu'
 
 require('bootstrap/dist/css/bootstrap.min.css')
 require('font-awesome/css/font-awesome.min.css')
@@ -8,6 +9,7 @@ require('font-awesome/css/font-awesome.min.css')
 
 const App = props => (
     <div className="container">
+        <Menu />
         <Todo />
         <About />
     </div>
