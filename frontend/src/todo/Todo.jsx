fcc746e4bd32b4c9710aabf19754e646c9014ebc
Roberto Fortes <robertbernd23@gmail.com>
2021-07-31 20:32:44 -0300

    Botão adicionar (Integrando Backend)

--- frontend/src/todo/Todo.jsx
+++ frontend/src/todo/Todo.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PageHeader from '../template/PageHeader'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
     constructor(props) {
@@ -18,7 +21,9 @@ export default class Todo extends Component {
     }
 
     handleAdd() {
-        console.log(this.state.description)
+        const description = this.state.description
+        axios.post(URL, { description })
+        .then(resp => console.log('Funcionou!'))
     }
     
     render(){
