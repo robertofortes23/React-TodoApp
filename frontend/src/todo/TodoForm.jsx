09cd934e5932379f052ca5feba7d6103c161b770
Roberto Fortes <robertbernd23@gmail.com>
2021-08-03 19:14:17 -0300

    Adicionando teclas de atalhos

--- frontend/src/todo/TodoForm.jsx
+++ frontend/src/todo/TodoForm.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import Grid from '../template/Grid'
 import IconButton from '../template/IconButton'
 
-const TodoForm = props => (
-    <div role='form' className='todoForm'>
+const TodoForm = props => {
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+
+    return (
+        <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa' onChange={props.handleChange} value={props.description}></input>
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa' onChange={props.handleChange} onKeyUp={keyHandler} value={props.description}></input>
         </Grid>
 
         <Grid cols='12 3 2'>
@@ -15,6 +24,7 @@ const TodoForm = props => (
             <IconButton btnStyle='default' icon='close' onClick={props.handleClear}/>
         </Grid>
     </div>
-)
+    )
+}
 
 export default TodoForm
